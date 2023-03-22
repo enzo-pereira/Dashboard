ef69f8413583c546849681d293cf6111739d04d4
Enzo <enzo.pereira@epitech.eu>
2023-03-22 15:40:05 +0100

    [fix] fixing gmail widget date

--- Dashboard-2/client-server/src/widgets/google/gmail.js
+++ Dashboard-2/client-server/src/widgets/google/gmail.js
@@ -2,33 +2,33 @@ const { google } = require('googleapis');
 const { oauth2Client } = require('../../Oauth2/GoogleAuth2');
 
 async function detectNewMail() {
-  const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
-
-  return new Promise((resolve, reject) => {
-    gmail.users.messages.list({ userId: 'me' }, (error, response) => {
-      if (error) {
-        console.log(error);
-        reject(error);
-        return;
-      }
-
-      const messageID = response.data.messages[0].id;
-
-      gmail.users.messages.get(
-        { userId: 'me', id: messageID },
-        (err, res) => {
-          if (err) {
-            console.log(err);
-            reject(err);
-            return;
-          }
-
-          const lastEmailDate = new Date(parseInt(res.data.internalDate));
-          resolve(lastEmailDate);
+    const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+ 
+    return new Promise((resolve, reject) => {
+      gmail.users.messages.list({ userId: 'me' }, (error, response) => {
+        if (error) {
+          console.log(error);
+          reject(error);
+          return;
         }
-      );
+ 
+        const messageID = response.data.messages[0].id;
+ 
+        gmail.users.messages.get(
+          { userId: 'me', id: messageID },
+          (err, res) => {
+            if (err) {
+              console.log(err);
+              reject(err);
+              return;
+            }
+ 
+            const lastEmailDate = new Date(parseInt(res.data.internalDate));
+            resolve(lastEmailDate);
+          }
+        );
+      });
     });
-  });
-}
-
-module.exports = { detectNewMail };
+  }
+ 
+  module.exports = { detectNewMail };
--- Dashboard-2/client-server/src/widgets/google/gmailroute.js
+++ Dashboard-2/client-server/src/widgets/google/gmailroute.js
@@ -3,11 +3,12 @@ const { detectNewMail } = require("./gmail")
 const { google } = require('googleapis');
 
 const gmail = express();
-
 gmail.get("/api/google/gmail", async (req, res) => {
-  const lastEmailDate = await detectNewMail();
-  console.log("Last email received on:", lastEmailDate);
-  res.send(lastEmailDate);
+  const lastEmailDate = new Date(await detectNewMail());
+  lastEmailDate.setHours(lastEmailDate.getHours() + 1);
+  const formattedDate = lastEmailDate.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'})
+  console.log("Last email received on:", formattedDate);
+  res.send(formattedDate.toLowerCase().replace(/\b(\w)/g, (match) => match.toUpperCase()));
 });
 
 module.exports = { gmail }
