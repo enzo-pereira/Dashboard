bc82b86080fe0a8387fb0b48a9d51308653fa67e
Enzo <enzo.pereira@epitech.eu>
2023-03-23 01:18:39 +0100

    [fix] crash with try catch

--- Dashboard-2/client-server/src/widgets/google/gmailroute.js
+++ Dashboard-2/client-server/src/widgets/google/gmailroute.js
@@ -1,14 +1,20 @@
 const express = require("express");
-const { detectNewMail } = require("./gmail")
+const { detectNewMail } = require("./gmail");
 const { google } = require('googleapis');
 
 const gmail = express();
+
 gmail.get("/api/google/gmail", async (req, res) => {
-  const lastEmailDate = new Date(await detectNewMail());
-  lastEmailDate.setHours(lastEmailDate.getHours() + 1);
-  const formattedDate = lastEmailDate.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'})
-  console.log("Last email received on:", formattedDate);
-  res.send(formattedDate.toLowerCase().replace(/\b(\w)/g, (match) => match.toUpperCase()));
+  try {
+    const lastEmailDate = new Date(await detectNewMail());
+    lastEmailDate.setHours(lastEmailDate.getHours() + 1);
+    const formattedDate = lastEmailDate.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'})
+    console.log("Last email received on:", formattedDate);
+    res.send(formattedDate.toLowerCase().replace(/\b(\w)/g, (match) => match.toUpperCase()));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
 });
 
-module.exports = { gmail }
+module.exports = { gmail };
--- Dashboard-2/client-server/src/widgets/meteo/meteo.js
+++ Dashboard-2/client-server/src/widgets/meteo/meteo.js
@@ -10,25 +10,30 @@ const routerForecast = express();
 const WEATHER_API = process.env.WEATHER_API;
 
 routerForecast.post("/forecast", isConnect, async (req, res) => {
-  const insee = req.body.insee;
-  await prisma.widget.create({
-    data: {
-      name: "Forecast",
-      description: `${req.body.insee}, probapluie`,
-      userId: req.user.userId,
-    },
-  });
+  try {
+    const insee = req.body.insee;
+    await prisma.widget.create({
+      data: {
+        name: "Forecast",
+        description: `${req.body.insee}, probapluie`,
+        userId: req.user.userId,
+      },
+    });
 
-  const forecast = await axios.get(
-    `https://api.meteo-concept.com/api/forecast/daily/0?token=${WEATHER_API}&insee=${insee}`
-  );
-  console.log(forecast.data.forecast);
-  const data = {
-    name: forecast.data.city.name,
-    probapluie: forecast.data.forecast.probarain,
-    tempmax: forecast.data.forecast.tmax,
-  };
-  res.status(200).send(data);
+    const forecast = await axios.get(
+      `https://api.meteo-concept.com/api/forecast/daily/0?token=${WEATHER_API}&insee=${insee}`
+    );
+    console.log(forecast.data.forecast);
+    const data = {
+      name: forecast.data.city.name,
+      probapluie: forecast.data.forecast.probarain,
+      tempmax: forecast.data.forecast.tmax,
+    };
+    res.status(200).send(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
 });
 
 module.exports = { routerForecast };
