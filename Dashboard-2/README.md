26447d51daeb78e089cdead529ed018a4b75be2a
Enzo <enzo.pereira@epitech.eu>
2023-03-26 17:49:30 +0200

    [ADD] add readme and fix crash

---
+++ Dashboard-2/README.md
@@ -0,0 +1,37 @@
+
+# Dashboard
+
+The purpose of this project is to implement a web application that works like Netvibes.
+
+## Services
+
+### Gmail
+
+#### Widget
+
+- See the last mail you receive.
+- See who send you a specify mail with a number.
+
+### Weather
+
+#### Widget
+
+- Current percentage chance of rainfall in a city.
+- Current maximum temperature of a city.
+
+### Reddit
+
+#### Widget
+
+- See the comment of a post Reddit
+
+
+## Installation and running
+
+To deploy and run this project do:
+
+```bash
+  docker compose up --build
+```
+
+The application web will run on "localhost:8081"
--- Dashboard-2/client-server/src/widgets/google/gmail.js
+++ Dashboard-2/client-server/src/widgets/google/gmail.js
@@ -2,18 +2,19 @@ const { google } = require('googleapis');
 const { oauth2Client } = require('../../Oauth2/GoogleAuth2');
 
 async function detectNewMail() {
-    const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+  const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
  
-    return new Promise((resolve, reject) => {
-      gmail.users.messages.list({ userId: 'me' }, (error, response) => {
-        if (error) {
-          console.log(error);
-          reject(error);
-          return;
-        }
- 
+  return new Promise((resolve, reject) => {
+    gmail.users.messages.list({ userId: 'me' }, (error, response) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+        return;
+      }
+
+      try {
         const messageID = response.data.messages[0].id;
- 
+
         gmail.users.messages.get(
           { userId: 'me', id: messageID },
           (err, res) => {
@@ -22,28 +23,34 @@ async function detectNewMail() {
               reject(err);
               return;
             }
- 
+
             const lastEmailDate = new Date(parseInt(res.data.internalDate));
             resolve(lastEmailDate);
           }
         );
-      });
+      } catch (error) {
+        console.log(error);
+        reject(error);
+      }
     });
-  }
+  });
+}
 
-  async function detectUserMail(num) {
-    const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
- 
-    return new Promise((resolve, reject) => {
-      gmail.users.messages.list({ userId: 'me' }, (error, response) => {
-        if (error) {
-          console.log(error);
-          reject(error);
-          return;
-        }
+async function detectUserMail(num) {
+  const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+
+  return new Promise((resolve, reject) => {
+    gmail.users.messages.list({ userId: 'me' }, (error, response) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+        return;
+      }
+
+      try {
         console.log(num);
         const messageID = response.data.messages[Number(num)].id;
-        
+
         gmail.users.messages.get(
           { userId: 'me', id: messageID },
           (err, res) => {
@@ -52,12 +59,17 @@ async function detectNewMail() {
               reject(err);
               return;
             }
+
             console.log(res.data.payload.headers[17].value)
             resolve(res.data.payload.headers[17].value);
           }
         );
-      });
+      } catch (error) {
+        console.log(error);
+        reject(error);
+      }
     });
-  }
- 
-  module.exports = { detectNewMail, detectUserMail};
+  });
+}
+
+module.exports = { detectNewMail, detectUserMail};
